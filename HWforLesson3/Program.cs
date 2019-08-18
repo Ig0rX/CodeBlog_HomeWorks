9d348300c84c31816417aacf5436a2143986af05
Igor Kovalenko <iksreason@mail.ru>
2019-08-18 21:11:38 +0300

    Добавлен решение по 3му уроку

---
+++ HWforLesson3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWforLesson3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Целочисленные
+            //Знаковые
+            sbyte sb = -1;
+            short sh = 100;
+            int i = 1_000_000;
+            long l = 1_000_000_000;
+            //Без знаковые
+            byte b = 100;
+            ushort us= 1000;
+            uint ui = 2_000_000;
+            ulong ul = 2_000_000_000;
+            #endregion
+            #region Дробные
+
+            #endregion
+        }
+    }
+}
